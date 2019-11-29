d4bef3861a47e0d03aec0ba8c3479323958721d4
Austin Lasseter <31504432+austinlasseter@users.noreply.github.com>
2019-11-29 11:08:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function myname(){
-    document.write("You da man!");  
-}
-
-function welcomeW(name){
-    document.write("Welcome " + name + "!");
-}
-
-function welcomeR(name){
-    return "Welcome " name "!";
+function popup() {
+alert("Hello World")
 }
