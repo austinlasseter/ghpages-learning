803ffd13763dadf41db086fdc10666209d721598
Austin Lasseter <31504432+austinlasseter@users.noreply.github.com>
2019-11-29 11:05:58 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function myname(){
+    document.write("You da man!");  
+}
+
+function welcomeW(name){
+    document.write("Welcome " + name + "!");
+}
+
+function welcomeR(name){
+    return "Welcome " name "!";
+}
